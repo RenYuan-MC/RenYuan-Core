08ae7601bb29724df2bf840051a75694141ad137
lRENyaaa <92320175+lRENyaaa@users.noreply.github.com>
2022-08-09 22:09:42 +0800

    [构建跳过] 修改 AntiUserName

--- src/main/java/ren/rymc/renyuancore/protect/AntiUserName.java
+++ src/main/java/ren/rymc/renyuancore/protect/AntiUserName.java
@@ -2,17 +2,17 @@ package ren.rymc.renyuancore.protect;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
+import org.bukkit.event.player.PlayerLoginEvent;
 import org.geysermc.floodgate.api.FloodgateApi;
 
 
 public class AntiUserName implements Listener {
 
     @EventHandler
-    public void onPlayerLogin(AsyncPlayerPreLoginEvent event) {
+    public void onPlayerLogin(PlayerLoginEvent event) {
         String Prefix = FloodgateApi.getInstance().getPlayerPrefix();
-        if (event.getName().startsWith(Prefix) && !FloodgateApi.getInstance().isFloodgatePlayer(event.getUniqueId())) {
-            event.disallow(AsyncPlayerPreLoginEvent.Result.KICK_OTHER, "[AntiUserName]\n非法的基岩版玩家名称\n请非基岩版玩家名称不要以" + Prefix + "开头");
+        if (event.getPlayer().getName().startsWith(Prefix) && !FloodgateApi.getInstance().isFloodgatePlayer(event.getPlayer().getUniqueId())) {
+            event.disallow(PlayerLoginEvent.Result.KICK_OTHER, "[AntiUserName]\n非法的基岩版玩家名称\n请非基岩版玩家名称不要以" + Prefix + "开头");
         }
     }
 }
